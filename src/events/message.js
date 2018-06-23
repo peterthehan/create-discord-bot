fa7d15a6d79af784ff55998bf7189b613ac2f4fc
Peter Han <peter.han.9493@gmail.com>
2018-06-23 01:53:55 -0400

    Handle commands from dm/group dm, run prettier

--- src/events/message.js
+++ src/events/message.js
@@ -1,15 +1,20 @@
 const config = require('../config.json');
 
-module.exports = (message) => {
+module.exports = message => {
   // uncomment for self bot
   // if (message.author.id !== message.client.user.id) {
   //   return;
   // }
 
-  // ignore bot messages and messages not from guild text channels
-  if (message.author.bot || message.channel.type !== 'text') {
+  // ignore bot messages
+  if (message.author.bot) {
     return;
   }
+
+  // ignore messages not from guild text channels
+  // if (message.channel.type !== 'text') {
+  //   return;
+  // }
 
   const mentionRegExp = RegExp(`^<@!?${message.client.user.id}>`);
   const noPrefix = !config.prefix || !message.content.startsWith(config.prefix);
@@ -25,17 +30,27 @@ module.exports = (message) => {
   let command;
   if (noMention) {
     args = message.content.split(' ');
-    command = args.shift().slice(config.prefix.length).toLowerCase();
+    command = args
+      .shift()
+      .slice(config.prefix.length)
+      .toLowerCase();
   } else {
-    args = message.content.replace(mentionRegExp, '').trim().split(' ');
+    args = message.content
+      .replace(mentionRegExp, '')
+      .trim()
+      .split(' ');
     command = args.shift().toLowerCase();
   }
 
   // check if command file exists
   try {
-    console.log(`${message.guild.name}#${message.channel.name}|${message.author.tag}: ${message.content}`);
+    console.log(
+      (message.channel.type === 'text'
+        ? `${message.guild.name}#${message.channel.name}|`
+        : '') + `${message.author.tag}: ${message.content}`
+    );
     require(`../commands/${command}`).run(message, args);
   } catch (error) {
     console.log(error);
   }
-}
+};
