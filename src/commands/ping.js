ef14cd50cd68b3b35b341bb2764bedd68eb49582
Peter Han <peter.han.9493@gmail.com>
2019-01-30 21:33:30 -0500

    Add properties to command structure

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,13 +1,17 @@
-exports.run = (message, args) => {
-  return message.channel
-    .send({ embed: { description: 'Pinging...' } })
-    .then(newMessage => {
-      newMessage.edit({
-        embed: {
-          title: 'Pong! 🏓',
-          description: `${newMessage.createdTimestamp -
-            message.createdTimestamp} ms`
-        }
+module.exports = {
+  failIfEmptyArgs: false,
+  deleteCommand: false,
+  run: message => {
+    return message.channel
+      .send({ embed: { description: 'Pinging...' } })
+      .then(newMessage => {
+        newMessage.edit({
+          embed: {
+            title: 'Pong! 🏓',
+            description: `${newMessage.createdTimestamp -
+              message.createdTimestamp} ms`
+          }
+        });
       });
-    });
+  }
 };
