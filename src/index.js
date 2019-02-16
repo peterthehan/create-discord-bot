92e433c9be13f4803b082c2ad7d68216f9179c92
Peter Han <peter.han.9493@gmail.com>
2019-02-16 16:31:46 -0500

    Reorder index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const { Client } = require('discord.js');
 const { token } = require('./config');
 
-// create bot
 const client = new Client();
-client.login(token);
 
-// load event handlers
 require('./util/loadEvents')(client);
+
+client.login(token);
