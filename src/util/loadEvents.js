8ee322aca260de69d489891a22f89eaa401f655b
Peter Han <peter.han.9493@gmail.com>
2019-02-16 16:32:35 -0500

    Only handle ready event once

--- src/util/loadEvents.js
+++ src/util/loadEvents.js
@@ -1,7 +1,7 @@
 const requireEvent = event => require(`../events/${event}`);
 
 module.exports = client => {
-  client.on('ready', () => requireEvent('ready')(client));
+  client.once('ready', () => requireEvent('ready')(client));
   client.on('message', requireEvent('message'));
   process.on('unhandledRejection', error =>
     requireEvent('unhandledRejection')(error)
