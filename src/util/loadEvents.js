cf1cd8f7ad95c78765320a1737dbcdd3997fef25
Peter Han <peter.han.9493@gmail.com>
2019-02-17 14:23:00 -0500

    Pass default args to listener fn

--- src/util/loadEvents.js
+++ src/util/loadEvents.js
@@ -3,7 +3,5 @@ const requireEvent = event => require(`../events/${event}`);
 module.exports = client => {
   client.once('ready', () => requireEvent('ready')(client));
   client.on('message', requireEvent('message'));
-  process.on('unhandledRejection', error =>
-    requireEvent('unhandledRejection')(error)
-  );
+  process.on('unhandledRejection', requireEvent('unhandledRejection'));
 };
