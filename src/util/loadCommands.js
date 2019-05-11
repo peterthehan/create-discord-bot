eaaa5ac6a5348b7c602b8ba0d269c27fbe164abb
Peter Han <peter.han.9493@gmail.com>
2019-05-11 01:37:41 -0400

    Resolve path dynamically to work with cli invoke

--- src/util/loadCommands.js
+++ src/util/loadCommands.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 const { Collection } = require('discord.js');
 
 module.exports = client => {
   const commands = new Collection();
 
-  fs.readdirSync('./src/commands')
+  fs.readdirSync(path.resolve(__dirname, '../commands'))
     .filter(file => file.endsWith('.js'))
     .forEach(file => {
       const command = require(`../commands/${file}`);
