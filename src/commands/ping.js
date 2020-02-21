59ba8b5ad707473d2fb34b2a9b49c0cc64ae8c47
Peter Han <peter.han.9493@gmail.com>
2020-02-20 20:45:43 -0500

    Add additional alias, change cooldown

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,11 +1,11 @@
 module.exports = {
   name: 'ping',
-  aliases: ['p'],
+  aliases: ['p', 'pong'],
   ownersOnly: true,
   guildOnly: false,
   requireArgs: false,
   deleteCommand: false,
-  cooldown: Infinity,
+  cooldown: 10,
   disabled: false,
   messageExecute: async (message, args) =>
     message.channel.send(`🏓 ${Math.round(message.client.ws.ping)} ms`)
