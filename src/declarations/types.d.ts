a41ab396fa58eacf257b1b4a0913d37cb41e2fe2
Peter Han <peter.han.9493@gmail.com>
2020-09-04 20:19:56 -0700

    Cleanup (#54)
    * Remove unnecessary package key
    
    * Destruct only necessary keys, remove unnecessary field types
    
    * Create answers type
    
    * Bump patch ver

--- src/declarations/types.d.ts
+++ src/declarations/types.d.ts
@@ -1,21 +1,20 @@
 import interfaces from "./interfaces";
 
-export declare type Step = {
-  message: string;
-  action: interfaces.StepAction;
+export declare type Answers = {
+  name: string;
+  token: string;
 };
 
 export declare type Package = {
   name: string;
   version: string;
   description?: string;
-  keywords?: string[];
-  homepage?: string;
-  repository?: { type: string; url: string };
-  license: string;
-  main?: string;
-  bin?: string | Record<string, string>;
   scripts?: Record<string, string>;
   dependencies?: Record<string, string>;
   devDependencies?: Record<string, string>;
+};
+
+export declare type Step = {
+  message: string;
+  action: interfaces.StepAction;
 };
