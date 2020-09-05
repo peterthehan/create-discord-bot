53669089f67044435c1a114266846606e88836a2
Peter Han <peter.han.9493@gmail.com>
2020-09-04 20:44:15 -0700

    Fix eslint issues

--- app/src/widgets/foobar/handlers/messageReactionAdd.js
+++ app/src/widgets/foobar/handlers/messageReactionAdd.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 module.exports = async (messageReaction, user) => {
   console.log("foobar: messageReactionAdd, delete the foobar folder");
 };
