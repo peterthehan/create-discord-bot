0e19702c57463e17d62e1541ed48d9ad0bb4c522
Peter Han <peter.han.9493@gmail.com>
2020-11-27 23:21:27 -0800

    Make type jsdoc consistent

--- src/index.js
+++ src/index.js
@@ -35,9 +35,7 @@ const validateName = require("validate-npm-package-name");
  */
 const getApplicationId = (token) => {
   try {
-    /**
-     * @type {string}
-     */
+    /** @type {string} */
     const response = execSync(
       `curl -s -X GET -H "Authorization: Bot ${token}" "https://discord.com/api/oauth2/applications/@me"`
     ).toString();
@@ -48,13 +46,9 @@ const getApplicationId = (token) => {
     return null;
   }
 };
-/**
- * @type {string}
- */
+/** @type {string} */
 const appDir = path.join(__dirname, "../app");
-/**
- * @type {Package}
- */
+/** @type {Package} */
 const appPackage = require(path.join(appDir, "package.json"));
 /** @type {Package} */
 const { name, version } = require(path.join(__dirname, "../package.json"));
@@ -81,14 +75,14 @@ prompts([
     message: "Application name?",
   },
 ])
-  .then(async (/** @type {{ name: string; }} */ { name }) => {
+  .then(async (/** @type {{ name: string }} */ { name }) => {
     const dir = path.resolve(name);
     const isUpdate = fs.existsSync(dir);
     /** @type {Step[]} */
     let steps;
 
     if (isUpdate) {
-      /** @type {{ update: boolean; }}  */
+      /** @type {{ update: boolean }}  */
       const { update } = await prompts([
         {
           type: "confirm",
@@ -113,7 +107,7 @@ prompts([
         },
       ];
     } else {
-      /** @type {{ token: string; }} */
+      /** @type {{ token: string }} */
       const { token } = await prompts([
         {
           type: "password",
